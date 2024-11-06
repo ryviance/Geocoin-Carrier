2e9f789cd92d34f776c306a323c8b2aad9a0e941
ryviance <eion.ling0@gmail.com>
2024-11-06 05:45:57 -0800

    L16 Dialog commit. Button alert

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-// todo
+// Create a button element
+const button = document.createElement('button');
+
+// Set the button text
+button.innerText = 'Click Me!';
+
+// Add an event listener to handle click events
+button.addEventListener('click', () => {
+    alert('You clicked the button!');
+});
+
+// Append the button to the body of the document
+document.body.appendChild(button);
